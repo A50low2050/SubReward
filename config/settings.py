6018f787e10ff79d573d6406f2fda955dc454153
Artem <artem.alshan@yandex.ru>
2025-12-16 22:49:08 +0300

    Переместил токен и группу в env файлах
    - Добавил python-dotenv для загрузки из переменных окружения
    - Добавил файл .env-example для последующего правильного клонирования репозитория

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,8 @@
+import os
 from dataclasses import dataclass
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 @dataclass
@@ -9,17 +13,15 @@ class BotConfig:
 
 
 def load_config() -> BotConfig:
-    token = '7019540848:AAEzxE2zA8AlV3VVQZ2iJAeLnRGSacFIJBE'
-    group = 'beautyserviceb2b/1'
-    group_id = '@beautyserviceb2b'
+    token = os.getenv("TOKEN")
+    group = os.getenv("GROUP")
+    group_id = os.getenv("GROUP_ID")
 
     if not token:
         raise ValueError("Токен не задан в .env файле!")
 
     return BotConfig(token=token, group=group, group_id=group_id)
 
-
-# Загружаем конфиг и получаем токен
 config = load_config()
 
 TOKEN = config.token
